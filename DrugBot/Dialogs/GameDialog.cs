73e0c925935c2be10dc9cdb5ae5b920ebae89d90
David Fox <me@iamdavidfox.com>
2016-12-13 19:54:30 -0500

    add first game dialog

---
+++ DrugBot/Dialogs/GameDialog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using DrugBot.Data;
+
+namespace DrugBot.Dialogs
+{
+    [Serializable]
+    public class GameDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+
+            if (message.Text.ToLower() == "play")
+            {
+            }
+            else
+            {
+                // mirror the text
+                await context.PostAsync($"You said: {message.Text}. Say \"Hi\" to start the conversation.");
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
