3681a6793a2d77c7328353049ab6f6560a5362c2
David Fox <me@iamdavidfox.com>
2016-12-26 15:12:47 -0500

    some poco's

---
+++ DrugBot/Common/Defaults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrugBot.Common
+{
+    public class Defaults
+    {
+        public readonly static int StartingMoney = 1000;
+        public readonly static int GameStartDay = 1;
+        public readonly static int LocationId = 1;
+    }
+}
---
+++ DrugBot/Common/GameState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrugBot.Common
+{
+    public class GameState
+    {
+        public bool IsGameOver { get; set; }
+    }
+}
